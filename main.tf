ddd84b1d144af77608ac450206c8d9b95d518049
Luka Baruškin <luka.baruskin@itmagination.com>
2022-06-09 15:57:33 +0200

    second try at fixing command

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "null_resource" "assign" {
   provisioner "local-exec" {
-    command = format("python3 scripts/crud-assign-lf-tags.py --data \"%s\"", jsonencode(var.assign))
+    command = format("python3 scripts/crud-assign-lf-tags.py --data '%s'", jsonencode(var.assign))
   }
 }
 
 
 resource "null_resource" "grant" {
   provisioner "local-exec" {
-    command = format("python3 scripts/grant-revoke-lf-tags.py --data \"%s\"", jsonencode(var.grants))
+    command = format("python3 scripts/grant-revoke-lf-tags.py --data '%s'", jsonencode(var.grants))
   }
   depends_on = [
     null_resource.assign
